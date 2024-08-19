3ed641705429c5272163ac3c44b6eb3cad5c11ae
MohmmedGaus Anwarali Shaikh <100345963+shaikhmohmmed@users.noreply.github.com>
2024-08-19 09:55:57 +0530

    Add files via upload
    This project is made in JavaScript DOM

---
+++ index.js
@@ -0,0 +1,26 @@
+let randomNumber1;
+
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+randomDice = "dice" + randomNumber1 + ".png";
+ let randomImageSource = "images/" + randomDice;
+
+ let image1 = document.querySelectorAll("img")[0];
+ image1.setAttribute("src", randomImageSource);
+
+ let randomNumber2 = Math.floor(Math.random() *6)+1;
+
+
+ let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+ let image2 = document.querySelectorAll("img")[1];
+ image2.setAttribute("src" , randomImageSource2);
+
+
+ if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+ }
+ else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML= "Player 2 wins";
+ }
+ else{
+   document.querySelector("h1").innerHTML = "Draw";
+ }
